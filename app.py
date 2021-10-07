bc626f33a17c28470a5c6eea87fdf9684cd61689
Debian <admin@ip-172-26-10-25.us-west-2.compute.internal>
2021-10-06 17:54:01 -0700

    update 2021/10/06 17:54:01

--- app.py
+++ app.py
@@ -260,11 +260,11 @@ def profile():
 @app.errorhandler(404)
 def page_not_found(e):
     # note that we set the 404 status explicitly
-    return render_template('404.html'), 404
+    return render_template('404.html', account=get_acc()), 404
 
 @app.errorhandler(500)
 def internal(e):
-    return render_template('500.html'), 500
+    return render_template('500.html', account=get_acc()), 500
 
 
 
